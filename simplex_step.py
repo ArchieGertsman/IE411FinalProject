0f18f4cfea831d371711071117753a0e550e64e0
archiegertsman <arkadiy2@illinois.edu>
2021-12-11 13:09:55 -0600

    completed simplex_step without thorough testing

--- simplex_step.py
+++ simplex_step.py
@@ -1,18 +1,59 @@
 import numpy as np
 
-def simplex_step(A, b, c, iB, iN, xB, B_inv, rule):
+
+
+def simplex_step(A, B_inv, b_bar, c, iB, rule):
     cB      = c[:,iB]
     w       = cB @ B_inv
-    b_bar   = B_inv @ b
     z       = cB @ b_bar
 
-    tableau = np.vstack((
-        np.hstack((w,     z    )),
-        np.hstack((B_inv, b_bar))
-    ))
-
     neg_reduced_costs = w@A - c
+    best_cost_idx, best_cost = \
+        select_pivot(neg_reduced_costs, rule)
 
+    if best_cost <= 0:
+        # at optimal solution
+        return (-1, iB, b_bar, B_inv)
+
+    y = B_inv @ A[:, best_cost_idx]
+    indices = np.argwhere(y > 0).reshape(-1)
+    if len(indices) == 0:
+        # unbounded
+        return (16, iB, b_bar, B_inv)
+
+    ratios = b_bar[indices] / y[indices]
+    min_ratio_idx = indices[ratios.argmin()]
+
+    tableau = np.vstack((
+        np.hstack([  w  ,   z  , best_cost]),
+        np.hstack([B_inv, b_bar[:,None],     y[:,None]    ])
+    ))
+    tableau = pivot(tableau, min_ratio_idx+1)
+    
+    iB[min_ratio_idx] = best_cost_idx
+    b_bar = tableau[1:,-2]
+    B_inv = tableau[1:, :b_bar.size]
+
+    return (0, iB, b_bar, B_inv)
+
+
+
+def pivot(tableau, idx):
+    # create a 1 in the last column of the pivot row
+    tableau[idx] /= tableau[idx, -1]
+
+    multipliers = tableau[:,-1].reshape(-1,1)
+
+    # so that pivot row is not modified in next operation
+    multipliers[idx] = 0.
+
+    # create zeros in the last column of all the other rows
+    tableau -= multipliers * tableau[idx]
+    return tableau
+
+
+
+def select_pivot(neg_reduced_costs, rule):
     if rule == 0:
         # Dantzig's rule
         c_best_idx = neg_reduced_costs.argmax()
@@ -24,49 +65,24 @@ def simplex_step(A, b, c, iB, iN, xB, B_inv, rule):
             c_best_idx = indices[0]
             c_best = neg_reduced_costs[:,c_best_idx]
         else:
+            c_best_idx = None
             c_best = 0
 
-    if c_best <= 0:
-        # print("done")
-        return (-1, iB, iN, xB, B_inv)
+    return (c_best_idx, c_best)
 
-    y = B_inv @ A[:, c_best_idx]
-    indices = np.argwhere(y > 0)
-    if len(indices) == 0:
-        # print("unbounded")
-        return (16, iB, iN, xB, B_inv)
-
-    ratios = b_bar[indices] / y[indices]
-    min_ratio_idx = indices[ratios.argmin()]
-
-    tableau = np.hstack((tableau, np.vstack(c_best, y)))
-    
-    # TODO: pivot at tableau[-1, min_ratio_idx]
-    # TODO: update iB, iN, xB, B_inv
-
-    return (0, iB, iN, xB, B_inv)
 
 
 
 if __name__ == "__main__":
-    A1 = np.matrix([[1, 1,  1],
-                [1, 1, -1],
-                [1, 1,  0]],dtype = np.float64)
+    A = np.array([
+        [1, 1,  1],
+        [1, 1, -1],
+        [1, 1,  0]],
+        dtype = np.float64)
+    A = np.hstack((np.eye(3),A))
+    B_inv = np.eye(3)
+    b_bar = np.array([1,2,3], dtype=np.float64)
+    c  = np.matrix([[0,0,0,-1,2,1]], dtype=np.float64)
+    iB = [0,1,2]
 
-    A = np.hstack((np.eye(3),A1))
-
-    b = np.matrix([[1],
-                [2],
-                [3]],dtype = np.float64)
-                
-
-    iB = [1,2,3]
-    iN = [4,5,6]
-    xB = np.matrix(np.copy(b))
-    c  = np.matrix([[0,0,0,-1,2,1]],dtype = np.float64)
-
-    B_inv = np.linalg.inv(A[:,iB])
-
-    # test a step in this extremely simple state
-    irule = 0
-    print(simplex_step(A,b,c,iB,iN,xB, B_inv, irule))
+    print(simplex_step(A, B_inv, b_bar, c, iB, rule=0))
