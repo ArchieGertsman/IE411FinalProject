c7ffbbd4173460c98a96be5c2c33e51470726235
archiegertsman <arkadiy2@illinois.edu>
2021-12-13 12:53:56 -0600

    started working on simplex_init

--- simplex_init.py
+++ simplex_init.py
@@ -1,7 +1,19 @@
+from simplex_method import simplex_method
 import numpy as np
 
-def simplex_init(A, b, c):
-    pass
+def simplex_init(A, b, rule):
+    m,n = A.shape
+    A = np.hstack((A, np.eye(m)))
+    c = np.hstack((np.zeros(n), np.ones(m)))
+    B_inv = np.eye(m)
+    iB = n + np.arange(m)
+
+    status, x, z, iB, b_bar = \
+        simplex_method(A, b, c, rule, B_inv, iB)
+
+    if iB[iB >= n].size > 0:
+        # infeasible
+        return (16, iB, np.zeros(m))
 
 
 if __name__ == "__main__":
